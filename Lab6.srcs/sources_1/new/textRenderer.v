22c5f4efba584638208fed8bb16b7c72eeb58946
Siwat_Keng <siwat.keng.pongpanit@gmail.com>
2020-05-22 21:18:15 +0700

    5 members

--- Lab6.srcs/sources_1/new/textRenderer.v
+++ Lab6.srcs/sources_1/new/textRenderer.v
@@ -6,17 +6,48 @@ module textRenderer(
     input [31:0] y,
     input clk
     );
-    
-        wire text;
-        Pixel_On_Text2 #(.displayText("603xxxxxxx Name... Lastname...")) t1(
+        wire member1, member2, member3, member4, member5;
+        Pixel_On_Text2 #(.displayText("603xxxxxxx Name... Lastname...1")) t1(
                 clk,
-                200, // text position.x (top left)
-                200, // text position.y (top left)
-                x, // current position.x
-                y, // current position.y
-                text  // result, 1 if current pixel is on text, 0 otherwise
+                200,
+                100,
+                x,
+                y,
+                member1 
             );
+        Pixel_On_Text2 #(.displayText("603xxxxxxx Name... Lastname...2")) t2(
+                clk,
+                200,
+                150,
+                x,
+                y,
+                member2
+            ); 
+        Pixel_On_Text2 #(.displayText("603xxxxxxx Name... Lastname...3")) t3(
+                clk,
+                200,
+                200,
+                x,
+                y,
+                member3
+            );
+        Pixel_On_Text2 #(.displayText("603xxxxxxx Name... Lastname...4")) t4(
+                clk,
+                200,
+                250,
+                x,
+                y,
+                member4
+            );
+        Pixel_On_Text2 #(.displayText("603xxxxxxx Name... Lastname...5")) t5(
+                clk,
+                200,
+                300,
+                x,
+                y,
+                member5
+            );                                               
             
-        assign video_on = text;
+        assign video_on = member1 || member2 || member3 || member4 || member5;
             
 endmodule
